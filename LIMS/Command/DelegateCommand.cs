7861eedaaa5a4c83310eba63b3bb91755eb95204
Jem Davis <jemd321@outlook.com>
2022-08-25 16:41:38 +0100

    addeding documentation

--- LIMS/Command/DelegateCommand.cs
+++ LIMS/Command/DelegateCommand.cs
@@ -8,6 +8,12 @@ namespace LIMS.Command
         private readonly Action<object> _execute;
         private readonly Func<object, bool> _canExecute;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// </summary>
+        /// <param name="execute">Action representing the command to be executed.</param>
+        /// <param name="canExecute">Function determining if the command is able to execute.</param>
+        /// <exception cref="ArgumentNullException">Throws if command provided is null.</exception>
         public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             ArgumentNullException.ThrowIfNull(execute);
@@ -15,9 +21,15 @@ namespace LIMS.Command
             _canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Event for when the ability of the command to execute has been changed.
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
-        public void RaiseCanExecuteChanged()
+        /// <summary>
+        /// Raise CanExecuteChanged event when the ability of the command to be executed may change.
+        /// </summary>
+        public virtual void RaiseCanExecuteChanged()
         {
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
